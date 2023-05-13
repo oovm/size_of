5763b2090c14c2be6905b8db237717589249c544
Aster <galaster@foxmail.com>
2023-05-13 23:55:03 +0800

    Add test crates

---
+++ projects/macro-test/src/lib.rs
@@ -0,0 +1,6 @@
+
+make_answer!();
+
+fn main() {
+    println!("{}", answer());
+}
--- projects/name_of/src/lib.rs
+++ projects/name_of/src/lib.rs
@@ -4,6 +4,11 @@
 #![doc(html_logo_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 #![doc(html_favicon_url = "https://raw.githubusercontent.com/oovm/shape-rs/dev/projects/images/Trapezohedron.svg")]
 
-mod errors;
+extern crate proc_macro;
 
-pub use crate::errors::{Error, Result};
+use proc_macro::TokenStream;
+
+#[proc_macro]
+pub fn make_answer(_item: TokenStream) -> TokenStream {
+    "fn answer() -> u32 { 42 }".parse().unwrap()
+}
